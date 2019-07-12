c03b4f2e1878caaa61f2bdbe86de48d9a3450050
afghany <knashanas@gmail.com>
2019-07-12 15:54:22 -0700

    first commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define EGAME-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "EGAMEURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
